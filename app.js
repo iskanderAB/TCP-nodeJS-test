a2ae8d2fc69e5d9872866077747e07030b340975
iskanderAB <iskanderabasi@gmail.com>
2021-10-28 04:06:12 +0100

    get mac and ip address

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const net = require("net");
+const {networkInterfaces }  = require("os")
 
 const server = net.createServer(soket => {
+    console.log(soket.remoteAddress); //get IP
+    console.log(list);
+    const list = networkInterfaces(); // get mac address 
     soket.write(" connecting session !!\n");
     soket.on("data", data => console.log(data.toString()));
 })
